cff8369a4b73946738dcb54b0f56bd2f9e69c2dc
Victor Miron <152219065+Kamoffee@users.noreply.github.com>
2024-03-18 15:50:11 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,1 @@
 # add your code here
-lst = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0
-       else str(i) for i in range(1, 101)]
-
-result = "\n".join(lst)
-print(result)
-
-
