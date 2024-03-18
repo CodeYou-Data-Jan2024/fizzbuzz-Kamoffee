a58d6fa9727fc6097f76663faf016447cfea6205
Andrei Miron <victormiron.home@gmail.com>
2024-03-18 15:40:17 -0400

    modified fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,8 @@
 # add your code here
 lst = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0
-       else i for i in range(1, 101)]
+       else str(i) for i in range(1, 101)]
 
-for items in lst:
-    print(items)
+result = "\n".join(lst)
+print(result)
+
 
