21fc97fa83ab1dc402069754b99846c47a157bf9
Andrei Miron <victormiron.home@gmail.com>
2024-03-20 19:18:38 -0400

    With comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,11 @@
 # add your code here
+
+# Creating a FIZZBUZZ in a list
+
 lst = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0
        else str(i) for i in range(1, 101)]
+
+# Join together the strings and get rid of quotes
 
 result = "\n".join(lst)
 print(result)
